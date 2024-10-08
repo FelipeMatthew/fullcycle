2a9d8b4f58fd4b18e30d6a5fc83a8fc71a8512e9
Felipe Matthew <felipematthewnascimento@gmail.com>
2024-10-07 21:34:51 -0300

    :books: SOLID: SRP Example

--- solid_principles/single_responsibility.ts
+++ solid_principles/single_responsibility.ts
@@ -23,3 +23,31 @@ class Course {
 }
 
 // * CLASSE FAZENDO MAIS DE UMA COISA - COMUNICANDO COM SISTEMAS FAZENDO MAIS DO QUE O OBJETIVO PRINCIPAL DELA, DEIXANDO ASSIM UMA CLASSE RUIM DE MANUTENÇÃO
+
+// Maneira certa
+// Classe para criar funcionario
+class Employee {
+  constructor(public name: string, public salary: number) {}
+}
+
+// Classe para calcular salário
+class SalaryCalculator {
+  calculate(employee: Employee): number {
+    return employee.salary
+  }
+}
+
+// Classe para salvar no bd
+class EmployeeRespository {
+  save(employee: Employee) : void {
+    console.log(`Saving ${employee.name} to the database`)
+  }
+}
+
+// usando essas classes 
+const employeeOne = new Employee("Felipe", 5000)
+const calculateSalary = new SalaryCalculator()
+const saveOnDB = new EmployeeRespository()
+
+console.log(`Salary: ${calculateSalary.calculate}`)
+saveOnDB.save(employeeOne)
